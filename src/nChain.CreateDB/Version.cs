5a48380d037e83d0ea57b5b4a072be6cc51d6f9a
Boštjan Vižin <bostjan.vizin@crea.si>
2021-02-04 07:59:22 +0100

    Initial import.

---
+++ src/nChain.CreateDB/Version.cs
@@ -0,0 +1,10 @@
+﻿// Copyright (c) 2020 Bitcoin Association
+
+namespace nChain.CreateDB
+{
+  public class Version
+  {
+    public int VersionId { get; set; }
+    public int Updating { get; set; }
+  }
+}
